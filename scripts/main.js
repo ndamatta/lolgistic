3e415f578264cad2c9dcf451c8503b1b395fa027
ndamatta <damatta.natanael@gmail.com>
2024-04-04 15:41:10 -0300

    add comments to separate sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
+// BURGER BUTTON
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#navLinks');
 
 burgerIcon.addEventListener('click', () => {
    navbarMenu.classList.toggle('is-active');
 })
+
+// API KEY
