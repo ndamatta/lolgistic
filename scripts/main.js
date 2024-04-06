f10e1a0b967a78eef842eea85a2d4781134397b2
ndamatta <damatta.natanael@gmail.com>
2024-04-05 21:42:26 -0300

    finish renderRankedInfo function

--- scripts/main.js
+++ scripts/main.js
@@ -19,17 +19,49 @@ function searchSummoner() {
   data();
 }
 function renderBasicInfo(data){
-  console.log(data)
   let html = `
   <figure class="image is-128x128">
     <img class="is-rounded" src="https://ddragon.leagueoflegends.com/cdn/14.7.1/img/profileicon/${data.profileIconId}.png" alt=""/>
   </figure>
   <h1 class="is-size-3">${data.name}</h1>
   <h2 class="subtitle">Level ${data.summonerLevel}</h2>`
-  const summonerBasicInfoSection = document.querySelector('#summonerBasicInfoSection');
-  summonerBasicInfoSection.innerHTML = html;
+  const summonerBasicInfoElement = document.querySelector('#summonerBasicInfoSection');
+  summonerBasicInfoElement.innerHTML = html;
 }
+function renderRankedInfo(data){
+  console.log(data)
+ let html = `
+ <div id="soloQ">
+ <figure class="image is-64x64">
+   <img class="is-rounded" src="${getTierIcon(data)}" alt="Emblem tier icon" />
+ </figure>
+ <p id="soloQ_rank">${data[0].tier} ${data[0].rank}</p>
+ <p id="soloQ_winrate">${data[0].wins}W | ${data[0].losses}L</p>
+ <p id="soloQ_winratep">${getWinrate(data[0].wins, data[0].losses)}%</p>
+</div>
 
+<div id="flexQ">
+<figure class="image is-64x64">
+ <img class="is-rounded" src="${getTierIcon(data)}" alt="Emblem tier icon" />
+</figure>
+<p id="flexQ_rank">${data[1].tier} ${data[1].rank}</p>
+<p id="soloQ_winrate">${data[1].wins}W | ${data[1].losses}L</p>
+<p id="soloQ_winratep">${getWinrate(data[1].wins, data[1].losses)}%</p>
+</div>`
+const summonerRankedInfoElement = document.querySelector('#rankedInfo')
+summonerRankedInfoElement.innerHTML = html;
+}
+function getTierIcon(data) {
+  const path = `/images/tier${  data[0].tier.toUpperCase()}.webp`
+  console.log(path)
+  return path;
+}
+function getWinrate(wins, losses) {
+  let total_games = wins + losses
+  let winrate = (wins / total_games) * 100
+  return winrate.toFixed(2)
+
+}
 async function data() {
   //Get basic summoner info
   const bySummonerName = "lol/summoner/v4/summoners/by-name";
@@ -43,8 +75,8 @@ async function data() {
   const URL_rankedInfo = `https://${getRegion()}/${byEntries}/${basicInfoJSON.id}?api_key=${API_KEY}`;
   const rankedInfo = await fetch(URL_rankedInfo);
   const rankedInfoJSON = await rankedInfo.json();
+  renderRankedInfo(rankedInfoJSON)
   
-  console.log(rankedInfoJSON)
 }
 
 
