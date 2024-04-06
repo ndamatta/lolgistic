cbfc8ca9a260e0553798e79fc0d9a6a7c9c0fdff
ndamatta <damatta.natanael@gmail.com>
2024-04-06 15:31:17 -0300

    put ranks in box, add functions to get ext. info

--- scripts/main.js
+++ scripts/main.js
@@ -7,13 +7,30 @@ burgerIcon.addEventListener("click", () => {
 });
 
 // API KEY
-const API_KEY = "RGAPI-0bd1745b-7014-4858-b178-cacb2b17ffc5";
+const API_KEY = "RGAPI-5426b02f-f413-401a-be6b-0397fa44805c";
 
 function getSummonerName() {
   return document.querySelector("#searchBar").value;
 }
-function getRegion() {
-  return document.querySelector("#searchBarSection select").value;
+function getRegion(option) {
+  // Will use same function for both, to get ranked info and matches info. For matches it groups the options
+  let regionSelectElement = document.querySelector("#searchBarSection select").value;
+  switch(option) {
+    case "basic":
+      return regionSelectElement
+    case "match":
+      return fetch('./json/regions.json')
+        .then(response => response.json())
+        .then(data => {
+          for (const server in data) {
+            if (data[server].url == regionSelectElement) {
+              return `${data[server].region}.api.riotgames.com`;
+            }
+          }
+          // If no match found, return null or appropriate fallback value
+          return null;
+        })
+  }
 }
 function searchSummoner() {
   data();
@@ -31,84 +48,118 @@ function renderBasicInfo(data){
 function renderRankedInfo(data){
   console.log(data)
  let html = `
- <div id="soloQ">
+ <div class="box" id="soloQ">
  <figure class="image is-64x64">
-   <img class="is-rounded" src="${getTierIcon(data)}" alt="Emblem tier icon" />
+   <img class="is-rounded" src="${getTierIcon(data, "soloQ")}" alt="Emblem tier icon" />
  </figure>
- <p id="soloQ_rank">${data[0].tier} ${data[0].rank}</p>
- <p id="soloQ_winrate">${data[0].wins}W | ${data[0].losses}L</p>
- <p id="soloQ_winratep">${getWinrate(data[0].wins, data[0].losses)}%</p>
+ <p id="soloQ_rank">${getSoloQTierRank(data)}</p>
+ <p id="soloQ_winrate">${getWinrate(data, "soloQ")}</p>
+ <p id="soloQ_winratep">${getWinrateP(data, "soloQ")}</p>
 </div>
 
-<div id="flexQ">
+<div class="box" id="flexQ">
 <figure class="image is-64x64">
- <img class="is-rounded" src="${getTierIcon(data)}" alt="Emblem tier icon" />
+ <img class="is-rounded" src="${getTierIcon(data, "flexQ")}" alt="Emblem tier icon" />
 </figure>
-<p id="flexQ_rank">${data[1].tier} ${data[1].rank}</p>
-<p id="soloQ_winrate">${data[1].wins}W | ${data[1].losses}L</p>
-<p id="soloQ_winratep">${getWinrate(data[1].wins, data[1].losses)}%</p>
+<p id="flexQ_rank">${getFlexTierRank(data, "flexQ")}</p>
+<p id="flexQ_winrate">${getWinrate(data, "flexQ")}</p>
+<p id="flexQ_winratep">${getWinrateP(data, "flexQ")}</p>
 </div>`
 const summonerRankedInfoElement = document.querySelector('#rankedInfo')
 summonerRankedInfoElement.innerHTML = html;
 }
-function getTierIcon(data) {
-  const path = `/images/tier${  data[0].tier.toUpperCase()}.webp`
-  console.log(path)
-  return path;
+function getTierIcon(data, queue) {
+  try {
+    switch(queue) {
+      case "soloQ":
+          const path0 = `/images/tier${data[0].tier.toUpperCase()}.webp`
+          return path0;
+      case "flexQ":
+          const path1 = `/images/tier${data[1].tier.toUpperCase()}.webp`
+          return path1;
+    }
+  }
+  catch {return `/images/tierIRON.webp`}
+  }
+function getWinrate(data, queue) {
+  try {
+    switch(queue) {
+      case "soloQ":
+        return `${data[0].wins}W | ${data[0].losses}L`
+      case "flexQ":
+        return `${data[1].wins}W | ${data[1].losses}L`
+    }
+    
+  }
+  catch {return ""}
 }
-function getWinrate(wins, losses) {
-  let total_games = wins + losses
-  let winrate = (wins / total_games) * 100
-  return winrate.toFixed(2)
-
+function getWinrateP(data, queue) {
+  try {
+    switch(queue){
+      case "soloQ":
+        let total_games0 = data[0].wins + data[0].losses
+        let winrate0 = (data[0].wins / total_games0) * 100
+        return `WR ${winrate0.toFixed(2)}`
+      
+      case "flexQ":
+        let total_games1 = data[1].wins + data[1].losses
+        let winrate1 = (data[1].wins / total_games1) * 100
+        return `WR ${winrate1.toFixed(2)}`
+    }
+  }
+  catch {return ""}
+}
+function getSoloQTierRank(data) {
+  try {
+    return `${data[0].tier} ${data[0].rank}`
+  }
+  catch {return "No ranked info available"}
+}
+function getFlexTierRank(data) {
+  try {
+    return `${data[1].tier} ${data[1].rank}`
+  }  
+  catch {
+    return "No ranked info available"
+  }
 }
 async function data() {
   //Get basic summoner info
   const bySummonerName = "lol/summoner/v4/summoners/by-name";
-  let URL_basicInfo = `https://${getRegion()}/${bySummonerName}/${getSummonerName()}?api_key=${API_KEY}`;
+  const URL_basicInfo = `https://${getRegion("basic")}/${bySummonerName}/${getSummonerName()}?api_key=${API_KEY}`;
   const basicInfo = await fetch(URL_basicInfo);
   const basicInfoJSON = await basicInfo.json();
-  renderBasicInfo(basicInfoJSON)
+  renderBasicInfo(basicInfoJSON);
 
   //Get ranked info
   const byEntries = "lol/league/v4/entries/by-summoner";
-  const URL_rankedInfo = `https://${getRegion()}/${byEntries}/${basicInfoJSON.id}?api_key=${API_KEY}`;
+  const URL_rankedInfo = `https://${getRegion("basic")}/${byEntries}/${basicInfoJSON.id}?api_key=${API_KEY}`;
   const rankedInfo = await fetch(URL_rankedInfo);
   const rankedInfoJSON = await rankedInfo.json();
-  renderRankedInfo(rankedInfoJSON)
-  
+  renderRankedInfo(rankedInfoJSON);
+
+  //Get matches info
+  const byPuuID = "lol/match/v5/matches/by-puuid"
+  const region = await getRegion("match");
+  const URL_matchesID = `https://${region}/${byPuuID}/${basicInfoJSON.puuid}/ids?start=0&count=20&api_key=${API_KEY}`;
+  const matchesID = await fetch(URL_matchesID);
+  const matchesIDJSON = await matchesID.json();
+  console.log(matchesIDJSON);
 }
-
-
 
 // Choose region
-const regions = {
-  "Brazil": "br1.api.riotgames.com",
-  "Europe Nordic & East": "eun1.api.riotgames.com",
-  "Europe West": "euw1.api.riotgames.com",
-  "Japan": "jp1.api.riotgames.com",
-  "Korea": "kr.api.riotgames.com",
-  "Latin America North": "la1.api.riotgames.com",
-  "Latin America South": "la2.api.riotgames.com",
-  "North America": "na1.api.riotgames.com",
-  "Oceania": "oc1.api.riotgames.com",
-  "Turkey": "tr1.api.riotgames.com",
-  "Russia": "ru.api.riotgames.com",
-  "Philippines": "ph2.api.riotgames.com",
-  "Singapore": "sg2.api.riotgames.com",
-  "Thailand": "th2.api.riotgames.com",
-  "Taiwan": "tw2.api.riotgames.com",
-  "Vietnam": "vn2.api.riotgames.com",
-};
-
 const selectElement = document.querySelector("#searchBarSection select");
 
-for (const region in regions) {
-  const option = document.createElement("option");
-  option.text = region;
-  option.value = regions[region];
-  selectElement.appendChild(option);
-}
+fetch('./json/regions.json')
+  .then(response => response.json())
+  .then(data => {
+    for (const server in data) {
+      const option = document.createElement("option");
+      option.text = `${data[server].short_name}`;
+      option.value = data[server].url;
+      selectElement.appendChild(option);
+    }
+  })
 
 document
   .querySelector("#searchBarBtn")
